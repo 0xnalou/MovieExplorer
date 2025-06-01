7894ce81c366019e00dbeb61a0889519f69f6767
Nalou Nguyen <long.nguyen@livepayments.com>
2025-06-01 21:20:37 +0700

    Initial Commit

---
+++ MovieExplorer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieExplorer
+//
+//  Created by Nalou Nguyen on 1/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovieExplorer/MovieExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieExplorerApp.swift
+//  MovieExplorer
+//
+//  Created by Nalou Nguyen on 1/6/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
